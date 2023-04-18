593ac10375712199bdaa6ba74a634e7b27690e0b
Francisco <fra.zelada.s@gmail.com>
2023-04-18 16:40:50 -0400

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { MovieProvider } from "@/context/movie";
+import { OrderProvider } from "@/context/order";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <OrderProvider>
+            <MovieProvider>
+                <Component {...pageProps} />
+            </MovieProvider>
+        </OrderProvider>
+    );
 }

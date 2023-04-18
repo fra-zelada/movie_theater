0a03180141fcf85a5e813c912826eba87896428b
Francisco <fra.zelada.s@gmail.com>
2023-04-18 16:37:54 -0400

    ticket validation page

---
+++ src/pages/ticket/[id].tsx
@@ -0,0 +1,38 @@
+import { PurchasedTicket } from "@/components";
+import { MainLayout } from "@/components/Layouts";
+import { OrderContext } from "@/context/order";
+import { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+
+interface TicketPageByIdProps {
+    host: string;
+}
+
+const TicketPageById: NextPage<TicketPageByIdProps> = ({ host }) => {
+    const router = useRouter();
+
+    const { id = "0" } = router.query;
+
+    const { getOrderById } = useContext(OrderContext);
+    const resp = getOrderById(`${id}`);
+
+    return (
+        <MainLayout>
+            {resp.status === "ok" && (
+                <div className="flex flex-col justify-center items-center">
+                    <PurchasedTicket order={resp.order!!} host={host} />
+                </div>
+            )}
+        </MainLayout>
+    );
+};
+
+export default TicketPageById;
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const host = ctx.req.headers.host || "";
+
+    return {
+        props: { host },
+    };
+};

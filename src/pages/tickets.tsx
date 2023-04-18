4bfa4197eadbfb640868c30d5aa5e6cd93b3d7c0
Francisco <fra.zelada.s@gmail.com>
2023-04-18 16:41:53 -0400

    my tickets

---
+++ src/pages/tickets.tsx
@@ -0,0 +1,50 @@
+import { MainLayout } from "@/components/Layouts";
+import { PurchasedTicket } from "@/components/PurchasedTicket";
+import { OrderContext } from "@/context/order";
+import { IOrder } from "@/interfaces";
+import { GetServerSideProps, NextPage } from "next";
+import { useContext, useEffect, useState } from "react";
+
+interface TicketsPageProps {
+    host: string;
+}
+
+const TicketsPage: NextPage<TicketsPageProps> = ({ host }) => {
+    const { getPaidOrders } = useContext(OrderContext);
+
+    const [tickets, setTickets] = useState<IOrder[]>([]);
+
+    useEffect(() => {
+        const resp = getPaidOrders();
+        if (resp.status === "ok") setTickets(resp.order!!);
+    }, [getPaidOrders]);
+
+    return (
+        <MainLayout>
+            <div className="flex justify-center items-center  w-full my-2  ">
+                <div className="flex flex-col xl:w-[60%]  justify-center items-center border-2 border-sky-900 bg-sky-950 rounded-lg shadow-xl p-4">
+                    {tickets.map((ticket) => (
+                        <PurchasedTicket
+                            key={ticket.id}
+                            order={ticket}
+                            host={host}
+                        />
+                    ))}
+                </div>
+            </div>
+        </MainLayout>
+    );
+};
+
+export default TicketsPage;
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const host = ctx.req.headers.host || "";
+
+    return {
+        props: { host },
+    };
+};
